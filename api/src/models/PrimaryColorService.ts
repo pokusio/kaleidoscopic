6aa62197346cb155144d6ae7d1a6bac4361c3c1e
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-10-11 08:02:19 +0200

    feat.(rest-api-get-and-post): now both get and post RESTEndpoints workon RESTAPI (epecially post, that I can invoke using JSON payload! :D )

--- api/src/models/PrimaryColorService.ts
+++ api/src/models/PrimaryColorService.ts
@@ -3,8 +3,16 @@ import * as dtree from 'directory-tree';
 import { ValidateErrorJSON } from './validate.json-error'
 
 // A post request should not contain any color_guid, it shallbe generated when new color is inserted into mongoDB
-export type PrimaryColorCreationParams = Pick<PrimaryColor,  "description" | "red" | "green" | "blue" | "opacity">;
-
+/// export type PrimaryColorCreationParams = Pick<PrimaryColor,  "description" | "red" | "green" | "blue" | "opacity">;
+export interface PrimaryColorCreationParams {
+  // color_id: number; /// this is the primary key in the database, auto-increment
+  // color_guid: string;/// 6c603f40-6abc-46ac-889e-08151658c7f7
+  description: string;
+  red: number;//"<0 to 255>"
+  green: number;//"<0 to 255>"
+  blue: number;//"<0 to 255>"
+  opacity: number;//"<0 to 1>" 2 decimals maximum (for CSS compatibility) let myNumber: number = 0.87;let myNumber: number = 10.8788;
+}
 
 export class PrimaryColorService {
 
